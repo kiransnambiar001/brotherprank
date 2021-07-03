4aaf48b73b56e6e5c35d89074c2fbb51d52e5c8d
kiransnambiar001 <kiransnambiar001@gmail.com>
2021-07-03 13:51:57 -0500

    dsa
    dsa

--- script.js
+++ script.js
@@ -2,5 +2,7 @@
 
 function button(){
     document.getElementById("image").src = "InkedPhantomForcesMan_LI.jpg";
+    document.getElementById("audionextto").src = "da.mp3"
     document.getElementById("audionextto").play();
+    document.getElementById("clicky").innerHTML = "oops i farted (minimize window)"
 }
